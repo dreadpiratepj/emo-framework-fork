b1b7c2df28d8219b685baa324f5a9d500c31c685
PJ Cabrera <pjcabrera@pobox.com>
2012-06-24 11:45:01 -0700

    Updated Emo-Android project to add version 0.8.2 of the Sqrat Squirrel binding library

--- Emo-Android/jni/Android.mk
+++ Emo-Android/jni/Android.mk
@@ -21,15 +21,16 @@ include $(LOCAL_PATH)/libpng/Android.mk
 include $(LOCAL_PATH)/sqlite/Android.mk
 include $(LOCAL_PATH)/Box2D/Android.mk
 include $(LOCAL_PATH)/rapidxml/Android.mk
+include $(LOCAL_PATH)/sqrat/Android.mk
 
 SQUIRREL_CFLAGS := -O0 -Wall -g
 
 APP_OPTIM       := release
 LOCAL_MODULE    := emo-android
 LOCAL_ARM_MODE  := arm
-LOCAL_SRC_FILES := native_app_glue.c main.cpp $(EMO_SRC_FILES) $(SQUIRREL_SRC_FILES) $(LIBPNG_SRC_FILES) $(SQLITE_SRC_FILES) $(BOX2D_SRC_FILES)
+LOCAL_SRC_FILES := native_app_glue.c main.cpp $(EMO_SRC_FILES) $(SQUIRREL_SRC_FILES) $(LIBPNG_SRC_FILES) $(SQLITE_SRC_FILES) $(BOX2D_SRC_FILES) $(SQRAT_SRC_FILES)
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM -lOpenSLES -lz
-LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/squirrel/include $(LOCAL_PATH)/libpng $(LOCAL_PATH)/sqlite $(LOCAL_PATH)/emo $(LOCAL_PATH)/Box2D $(LOCAL_PATH)/rapidxml
+LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/squirrel/include $(LOCAL_PATH)/libpng $(LOCAL_PATH)/sqlite $(LOCAL_PATH)/emo $(LOCAL_PATH)/Box2D $(LOCAL_PATH)/rapidxml $(LOCAL_PATH)/sqrat
 LOCAL_CFLAGS    := $(SQUIRREL_CFLAGS) $(SQLITE_CFLAGS)
 
 include $(BUILD_SHARED_LIBRARY)
---
+++ Emo-Android/jni/sqrat/Android.mk
@@ -0,0 +1,4 @@
+SQRAT_SRC_FILES :=\
+	sqrat/MySqratBindings.cpp \
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/sqrat $(LOCAL_PATH)/sqrat/sqrat
